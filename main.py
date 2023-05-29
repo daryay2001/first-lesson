ee7772ed757388afca2b4c5e158cba8a8b11b6c7
Daria_Yakymova <kiritocun.ru@gmail.com>
2023-05-29 20:43:32 +0300

    Added theory

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+# Условные конструкции и операторы сравнения
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# <
+# >
+# >=
+# <=
+# ==
+# !=
+# not
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Операторы бывают унарные и бинарные.
+# Унарные - у него один операнд, бинарные - у него два операнда,
+# т.е. то, над чем проводится операция
