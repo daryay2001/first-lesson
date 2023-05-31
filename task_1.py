32c609919aa7d2713fb36c512715205471d9ae0c
Daria_Yakymova <kiritocun.ru@gmail.com>
2023-05-31 11:17:34 +0300

    Added task 1

---
+++ task_1.py
@@ -0,0 +1,30 @@
+# 1. Пользователь вводит с клавиатуры три числа. В зависимости от выбора пользователя
+# программа выводит на экран максимум из трёх, минимум из трёх или среднеарифметическое трёх чисел.
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+# num3 = int(input("Enter third number: "))
+#
+# user_choice = int(input("Enter 1 for max, enter 2 for min, enter 3 for average: "))
+#
+# if user_choice == 1:
+#     if num1 >= num2 and num1 >= num3:
+#         print(f"max: {num1}")
+#     elif num2 >= num1 and num2 >= num3:
+#         print(f"max: {num2}")
+#     else:
+#         print(f"max: {num3}")
+# elif user_choice == 2:
+#     if num1 <= num2 and num1 <= num3:
+#         print(f"min: {num1}")
+#     elif num2 <= num1 and num2 <= num3:
+#         print(f"min: {num2}")
+#     else:
+#         print(f"min: {num3}")
+# elif user_choice == 3:
+#     aver = (num1 + num2 + num3)/3
+#     print(f"average: {aver}")
+# else:
+#     print("Please enter 1, 2 or 3")
+
+
