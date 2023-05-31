f36b66acc620cb8c1c9d94a4fa131e49c0cf36d6
Daria_Yakymova <kiritocun.ru@gmail.com>
2023-05-31 16:49:09 +0300

    added task 2

---
+++ task_2.py
@@ -0,0 +1,24 @@
+# Пользователь вводит с клавиатуры количество метров.
+# В зависимости от выбора пользователя программа переводит метры в мили, дюймы или ярды.
+# Метры в мили: метры / на 1609
+# Метры в дюймы: метры * на 39,37
+# Метры в ярды: метры * на 1,094
+
+# meters = int(input("Enter meters: "))
+# user_choice = int(input("Enter 1 for miles, enter 2 for inches, enter 3 for yards: "))
+# if user_choice == 1:
+#     result = meters/1609
+#     print(f"{meters} meters = {result} miles")
+# elif user_choice == 2:
+#     result = meters * 39.37
+#     print(f"{meters} meters = {result} inches")
+# elif user_choice == 3:
+#     result = meters * 1.094
+#     print(f"{meters} meters = {result} yards")
+# else:
+#     print("Please, enter 1, 2 or 3")
+
+
+
+
+
